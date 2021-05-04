3fd575cd0d9c5a0bb946f56de3435c1e323d3576
Ayano Taki <ayanobayern2@gmail.com>
2021-05-04 20:14:51 +0900

    firebaseプロジェクト変更

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,13 +1,13 @@
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD58Ooq8FngGw1rQtIhopbPAf0_gG_aDIk",
-    authDomain: "react-todolist-38368.firebaseapp.com",
-    databaseURL: "https://react-todolist-38368.firebaseio.com",
-    projectId: "react-todolist-38368",
-    storageBucket: "react-todolist-38368.appspot.com",
-    messagingSenderId: "568776363257",
-    appId: "1:568776363257:web:890d97a7348246ddd6a628",
-    measurementId: "G-77Y28SKT2R"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
-  
+
   export default firebaseConfig
